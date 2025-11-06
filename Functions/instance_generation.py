d546ea0d83b1b346756a94d1ff71b04dd384446c
holcrofm9664 <m.holcroft@lancaster.ac.uk>
2025-11-06 16:06:31 +0000

    Instances now as a dictionary for kwargs rather than a list

--- Functions/instance_generation.py
+++ Functions/instance_generation.py
@@ -1,9 +1,8 @@
-from Functions.Orders_Generation import generate_orders
+from Functions.orders_generation import generate_orders
 
 def generate_instances(A:list, B:list, O:list, Q:list, slot_capacity:int, between_aisle_dist:float, between_bay_dist:float):
 
-    combinations = {}
-    count = 1
+    combinations = []
 
     for a in A:
         for b in B:
@@ -11,8 +10,14 @@ def generate_instances(A:list, B:list, O:list, Q:list, slot_capacity:int, betwee
             for o in O:
                 for q in Q:
                     orders = generate_orders(o, q, num_products)
-                    combinations[count] = [a, b, slot_capacity, between_aisle_dist, between_bay_dist, orders]
-                    count += 1
+                    instance = {}
+                    instance["num_aisles"] = a
+                    instance["num_bays"] = b
+                    instance["slot_capacity"] = slot_capacity
+                    instance["between_aisle_dist"] = between_aisle_dist
+                    instance["between_bay_dist"] = between_bay_dist
+                    instance["orders"] = orders
+                    combinations.append(instance)
     
     return combinations
     
