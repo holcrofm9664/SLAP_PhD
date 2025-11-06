8b254e2f91a38bbf47dbd9ae32165704c63180a4
holcrofm9664 <m.holcroft@lancaster.ac.uk>
2025-11-06 16:02:31 +0000

    Have adjusted orders generation to use random sampling without replacement using the random package

--- Functions/orders_generation.py
+++ Functions/orders_generation.py
@@ -9,10 +9,7 @@ def generate_orders(num_orders:int, order_size:int, num_products:int) -> dict:
     for order in range(num_orders):
         order = []
         prod_list = [y for y in range(1, num_products + 1)]
-        while len(order) < order_size:
-            idx = random.randint(0, len(prod_list) - 1)
-            order.append(prod_list[idx])
-            prod_list.pop(idx)
+        order = random.sample(prod_list, order_size)
         Orders[count] = order
         count += 1
 
