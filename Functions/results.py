23ffcb5ab9d619da5375950c149d8aec6c6c363c
holcrofm9664 <m.holcroft@lancaster.ac.uk>
2025-11-10 14:22:41 +0000

    Fully commented and given type hints for the functions used in testing the model

--- Functions/results.py
+++ Functions/results.py
@@ -61,7 +61,24 @@ def test_model(model:Callable, A:list, B:list, O:list, Q:list, num_trials:int, s
     return df
 
 
-def test_ABO(A, B, O, Q_values, df, trials, model):
+def test_ABO(A:int, B:int, O:int, Q_values:list, df:pd.DataFrame, trials:int, model:Callable) -> pd.DataFrame:
+    """
+    Runs a model on all selected instances for fixed number of aisles, fixed number of bays and fixed number of orders
+    
+    Inputs:
+    - A: the number of aisles on which we wish to test our model, which is fixed
+    - B: the number of bays on which we wish to test our model, which is fixed
+    - O: the number of orders on which we wish to test our model, which is fixed
+    - Q_values: the list of order sizes on which we wish to test our model
+    - df: the current dataframe, containing parameters, distance and runtime of previously solved instances
+    - trials: the number of times we choose to run the model on each instance
+    - model: the model we wish to test
+
+    Output:
+    - df: the dataframe of instances solved so far, including parameters, distance and runtime
+    
+    
+    """
     for q in Q_values:
         if q > 2*A*B:
             break
@@ -83,7 +100,23 @@ def test_ABO(A, B, O, Q_values, df, trials, model):
     return df
     
 
-def test_AB(A, B, O_values, Q_values, df, trials, model):
+def test_AB(A:int, B:int, O_values:list, Q_values:list, df:pd.DataFrame, trials:int, model:Callable) -> pd.DataFrame:
+    """
+    Runs a model on all selected instances for fixed number of aisles and number of bays
+    
+    Inputs:
+    - A: the number of aisles on which we wish to test our model, which is fixed
+    - B: the number of bays on which we wish to test our model, which is fixed
+    - O_values: the list of number of orders on which we wish to test our model
+    - Q_values: the list of order sizes on which we wish to test our model
+    - df: the current dataframe, containing parameters, distance and runtime of previously solved instances
+    - trials: the number of times we choose to run the model on each instance
+    - model: the model we wish to test
+
+    Output:
+    - df: the dataframe of instances solved so far, including parameters, distance and runtime
+    
+    """
     for o in O_values:
         df_len = len(df)
         df = test_ABO(A, B, o, Q_values, df, trials, model)
@@ -92,7 +125,23 @@ def test_AB(A, B, O_values, Q_values, df, trials, model):
     return df
 
 
-def test_A(A, B_values, O_values, Q_values, df, trials, model):
+def test_A(A:int, B_values:list, O_values:list, Q_values:list, df:pd.DataFrame, trials:int, model:Callable) -> pd.DataFrame:
+    """
+    Runs a model on all selected instances for a fixed number of aisles
+
+    Inputs:
+    - A: the number of aisles on which we wish to test our model, which is fixed
+    - B_values: the list of number of bays on which we wish to test our model
+    - O_values: the list of number of orders on which we wish to test our model
+    - Q_values: the list of order sizes on which we wish to test our model
+    - df: the current dataframe, containing parameters, distance and runtime of previously solved instances
+    - trials: the number of times we choose to run the model on each instance
+    - model: the model we wish to test
+
+    Output:
+    - df: the dataframe of instances solved so far, including parameters, distance and runtime
+    """
+
     for b in B_values:
         df_len = len(df)
         df = test_AB(A, b, O_values, Q_values, df, trials, model)
@@ -101,7 +150,23 @@ def test_A(A, B_values, O_values, Q_values, df, trials, model):
     return df
 
 
-def test_all(A_values, B_values, O_values, Q_values, df, trials, model):
+def test_all(A_values:list, B_values:list, O_values:list, Q_values:list, trials:int, model:Callable) -> pd.DataFrame:
+    """
+    Runs a model on all selected instances, with the elimination of instances on which the model is known beforehand it will time out on.
+
+    Inputs:
+    - A_values: a list of number of aisles which we wish to test our model on
+    - B_values: a list of number of bays which we wish to test our model on
+    - O_values: a list of number of orders which we wish to test our model on
+    - Q_values: a list of order sizes which we wish to test our model on
+    - trials: the number of times we wish to run our model on each instance
+    - model: the model whose performance we wish to test
+
+    Outputs:
+    - df: a dataframe containing the instance parameters as well as average distance and runtime
+    """
+
+    df = pd.DataFrame()
     for a in A_values:
         df_len = len(df)
         df = test_A(a, B_values, O_values, Q_values, df, trials, model)
