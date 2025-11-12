e355cc33ba9e2b7c78bff4db0c03f68029a0be1c
holcrofm9664 <m.holcroft@lancaster.ac.uk>
2025-11-12 12:28:11 +0000

    Added function to build the distance matrix

--- Functions/distance_matrix_generation.py
+++ Functions/distance_matrix_generation.py
@@ -4,7 +4,11 @@ def calculate_aisle(i, num_bays):
     return np.ceil(i/num_bays)
 
 
-def dist_i_j_strict_s_shape(i, j, M, N, num_bays):
+def dist_i_j_strict_s_shape(i, j, **kwargs):
+    num_bays = kwargs["num_bays"]
+    M = kwargs["between_aisle_dist"]
+    N = kwargs["between_bay_dist"]
+
     aisle_i = calculate_aisle(i, num_bays)
     aisle_j = calculate_aisle(j, num_bays)
     L = N*(num_bays+1)
@@ -14,5 +18,23 @@ def dist_i_j_strict_s_shape(i, j, M, N, num_bays):
         return np.inf
     elif (aisle_j - aisle_i) % 2 == 0:
         return (aisle_j - aisle_i)*M + 2*L
-    elif (aisle_j - aisle_i) %2 == 1:
+    elif (aisle_j - aisle_i) % 2 == 1:
         return (aisle_j - aisle_i)*M + L
+
+
+def build_distance_matrix(pairwise_distance_function, **kwargs):
+    num_aisles = kwargs["num_aisles"]
+    num_bays = kwargs["num_bays"]
+    slot_capacity = kwargs["slot_capacity"]
+    M = kwargs["between_aisle_dist"]
+    N = kwargs["between_bay_dist"]
+
+    total_slots = num_aisles*num_bays*slot_capacity
+
+    d = np.zeros((num_aisles, num_bays))
+
+    for i in range(total_slots):
+        for j in range(total_slots):
+            d[i,j] = pairwise_distance_function(i, j, **kwargs)
+
+    return d
