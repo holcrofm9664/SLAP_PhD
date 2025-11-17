9f1778e12ab4dcf8d31a1b48bfc76c49815ede23
holcrofm9664 <m.holcroft@lancaster.ac.uk>
2025-11-17 11:07:21 +0000

    Removed simple function, improved inputs from **kwargs, improved commenting

--- Functions/distance_matrix_generation.py
+++ Functions/distance_matrix_generation.py
@@ -1,16 +1,27 @@
 import numpy as np
+from typing import Callable, Any
 
-def calculate_aisle(i, num_bays):
-    return np.ceil(i/num_bays)
+def dist_i_j_strict_s_shape(i:int, j:int, num_bays:int, between_aisle_dist:float, between_bay_dist:float, **unused:Any) -> int:
+    """
+    Given two slot indices i and j, calculated the distance from i to j 
+    for the strict S-shape policy (note that this is not symmetric due to unidirectionality in the warehouse)    
 
+    Inputs:
+    - i: the index of the first slot
+    - j: the index of the second slot
+    - num_bays: the number of bays per aisle in the warehouse
+    - between_aisle_dist: the distance between consecutive aisles in the warehouse
+    - between_bay_dist: the distance between consecutive bays in the warehouse
 
-def dist_i_j_strict_s_shape(i, j, **kwargs):
-    num_bays = kwargs["num_bays"]
-    M = kwargs["between_aisle_dist"]
-    N = kwargs["between_bay_dist"]
+    Outputs:
+    - the distance between the two bays
+    """
 
-    aisle_i = calculate_aisle(i, num_bays)
-    aisle_j = calculate_aisle(j, num_bays)
+    M = between_aisle_dist
+    N = between_bay_dist
+
+    aisle_i = np.ceil(i/num_bays)
+    aisle_j = np.ceil(j/num_bays)
     L = N*(num_bays+1)
     if aisle_i == aisle_j:
         return 0
@@ -22,19 +33,26 @@ def dist_i_j_strict_s_shape(i, j, **kwargs):
         return (aisle_j - aisle_i)*M + L
 
 
-def build_distance_matrix(pairwise_distance_function, **kwargs):
-    num_aisles = kwargs["num_aisles"]
-    num_bays = kwargs["num_bays"]
-    slot_capacity = kwargs["slot_capacity"]
-    M = kwargs["between_aisle_dist"]
-    N = kwargs["between_bay_dist"]
+def build_distance_matrix(pairwise_distance_function:Callable, num_aisles:int, num_bays:int, slot_capacity:int, between_aisle_dist:float, between_bay_dist:float, **unused:Any):
+    """
+    Calculate the distance matrix for a particular warehouse type given its input parameters
 
+    Inputs:
+    - pairwise_distance_function: the function used to calculate pairwise distances between two slots in the warehouse, depending on the warehouse type
+    - num_aisle: the number of aisles in the warehouse
+    - num_bays: the number of bays per aisle in the warehouse
+    - slot_capacity: the capacity of each slot in the warehouse. The standard is 2
+    - between_aisle_dist: the distance between consecutive aisles in the warehouse
+    - between_bay_dist: the distance between consecutive bays in the warehouse
+    
+    """
+    
     total_slots = num_aisles*num_bays*slot_capacity
 
-    d = np.zeros((num_aisles, num_bays))
+    d = np.zeros((total_slots, total_slots))
 
-    for i in range(total_slots):
-        for j in range(total_slots):
-            d[i,j] = pairwise_distance_function(i, j, **kwargs)
+    for i in range(1, total_slots+1):
+        for j in range(1, total_slots+1):
+            d[i-1,j-1] = pairwise_distance_function(i, j, num_bays, between_aisle_dist, between_bay_dist)
 
     return d
