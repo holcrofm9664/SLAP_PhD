b5c9c0005baab92dbf641198f4b9eced047c3675
holcrofm9664 <m.holcroft@lancaster.ac.uk>
2026-01-12 16:29:16 +0000

    Added a new instance generation function which outputs instances as tuples, allowing them to be used by starmap for parallelisation

--- functions/instance_generation.py
+++ functions/instance_generation.py
@@ -1,7 +1,7 @@
 from functions.orders_generation import generate_orders
 import pandas as pd
 
-def generate_instances(A:list, B:list, O:list, Q:list, slot_capacity:int, between_aisle_dist:float, between_bay_dist:float, seed:int, product_df:pd.DataFrame, crushing_multiple:float, backtrack_penalty:int, time_limit:int) -> list:
+def generate_instances_parallelisation(A:list, B:list, O:list, Q:list, slot_capacity:int, between_aisle_dist:float, between_bay_dist:float, seed:int, product_df:pd.DataFrame, crushing_multiple:float, backtrack_penalty:int, time_limit:int) -> tuple:
     """
     Generates all possible combinations of inputs given the user inputs which parameter values they would like to test
 
@@ -33,6 +33,52 @@ def generate_instances(A:list, B:list, O:list, Q:list, slot_capacity:int, betwee
                     if num_products >= q: # ensures that combinations are only generated for instances for which there are enough products to fill a single order
                         orders = generate_orders(num_orders = o, order_size = q, num_products = num_products, seed=seed)
 
+                        cluster_max_dist = b/2
+
+                        instance = (product_df, orders, a, b, slot_capacity, between_aisle_dist, between_bay_dist, crushing_multiple, cluster_max_dist, backtrack_penalty, time_limit)
+
+                        combinations.append(instance)
+    
+    return combinations
+    
+
+
+
+
+
+def generate_instances_kwargs(A:list, B:list, O:list, Q:list, slot_capacity:int, between_aisle_dist:float, between_bay_dist:float, seed:int, product_df:pd.DataFrame, crushing_multiple:float, backtrack_penalty:int, time_limit:int) -> list[dict]:
+    """
+    Generates all possible combinations of inputs given the user inputs which parameter values they would like to test
+
+    Inputs:
+    - A: a list containing the different aisle numbers that will be tested
+    - B: a list containing the different bay numbers that will be tested
+    - O: a list containing the different numbers of orders that will be tested
+    - Q: a list containing the different order sizes that will be tested
+    - slot_capacity: the capacity of slots in the warehouse. The standard is 2
+    - between_aisle_dist: the distance between two consecutive aisles
+    - between_bay_distance: the distance between two consecutive bays
+    - seed: the seed used in generating the orders
+    - product_df: a pandas dataframe containing the product ids and attributes such as weight and cluster
+    - crushing_multiple: the amount by which one product has to be heavier than another to crush it, as a multiple of the lighter product
+    - backtrack_penalty: the penalty applied for going against the one-way system
+    - time_limit: the time limit set for the assignment of products to aisles
+
+    Outputs:
+    - combinations: a list of dictionaries, where each dictionary constitutes an instance
+    """
+    
+    combinations = []
+
+    for a in A:
+        for b in B:
+            num_products = a * b * slot_capacity
+            for o in O:
+                for q in Q:
+                    if num_products >= q: # ensures that combinations are only generated for instances for which there are enough products to fill a single order
+                        orders = generate_orders(num_orders = o, order_size = q, num_products = num_products, seed=seed)
+
+                        
                         instance = {}
                         instance["num_aisles"] = a
                         instance["num_bays"] = b
@@ -49,7 +95,9 @@ def generate_instances(A:list, B:list, O:list, Q:list, slot_capacity:int, betwee
                         combinations.append(instance)
     
     return combinations
-    
+
+
+
 
 
 def generate_single_instance(num_aisles:int, num_bays:int, slot_capacity:int, between_aisle_dist:float, between_bay_dist:float, orders:dict) -> dict:
