5d60edc4c444af376b939e7c53aebeca198c945a
holcrofm9664 <m.holcroft@lancaster.ac.uk>
2026-01-20 15:05:12 +0000

    Added type hints and commenting

--- functions/generating_product_df.py
+++ functions/generating_product_df.py
@@ -2,8 +2,22 @@ import pandas as pd
 import numpy as np
 import random
 
-def create_data_df(num_prods ,weight_lower, weight_upper, num_clusters, weights_dp = 2, seed = 123):
+def create_data_df(num_prods:int ,weight_lower:float, weight_upper:float, num_clusters:int, weights_dp:int = 2, seed:int = 123) -> None:
+    """
+    Creates a synthetic product dataframe including product id, weight and cluster
 
+    Inputs:
+    - num_prods: the number of products we wish to have in our warehouse. Usually, this is equal to the number of slots (num_aisles * num_bays * slot_capacity)
+    - weights_lower: the minimum weight we would like to assign to a product
+    - weights_upper: the maximum weight we would like to assign to a product
+    - num_clusters: the number of clusters we would like our products to be divided across
+    - weights_dp: the number of decimal places we would like for products to be rounded to. The standard is 2
+    - seed: the seed used to generate the product attributes. The standard is 123
+
+    Outputs:
+    - create a pandas dataframe with product id, weight and cluster, and saves this as a .parquet file
+    """
+    
     random.seed(seed)
     
     prod_ids = np.arange(1, num_prods+1)
